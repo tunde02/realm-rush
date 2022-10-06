7d8b552809ddfb366ae2eefede68ec10a7352c7c
SeongHo Jin <snskrktja@naver.com>
2022-10-07 01:09:48 +0900

    부드러운 적 움직임 구현

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField]
+    List<Waypoint> path = new List<Waypoint>();
+
+    [SerializeField]
+    [Range(0f, 5f)]
+    float speed = 1f;
 
     void Start()
     {
@@ -16,8 +20,19 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
